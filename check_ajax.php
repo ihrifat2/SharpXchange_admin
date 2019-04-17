17f85235a3b8c26275873a7f42ccd71b241074dd
Imran Hadid <imranhadid03@gmail.com>
2019-04-17 22:36:35 +0600

    add real time notification

--- check_ajax.php
+++ check_ajax.php
@@ -1,5 +1,18 @@
 <?php
 
+require 'pusher/vendor/autoload.php';
+
+$options = array(
+    'cluster' => 'ap2',
+    'useTLS' => true
+);
+$pusher = new Pusher\Pusher(
+    'c0d39fd7bd9c14eb2b6a',
+    '8c4db4e31baead871afd',
+    '764369',
+    $options
+);
+
 require "adminsqlhelper.php";
 if (isset($_POST['exchangeGw'])) {
 	$exchangeGw = $_POST['exchangeGw'];
@@ -10,9 +23,14 @@ if (isset($_POST['activeStatus'])) {
 	$activeStatus = $_POST['activeStatus'];
 	if ($activeStatus == "on") {
 		$activeStatus = 1;
+		$data['message'] = 2;
+    	$pusher->trigger('sharpxchange', 'notification', $data);
 	} else {
 		$activeStatus = 0;
+		$data['message'] = 3;
+    	$pusher->trigger('sharpxchange', 'notification', $data);
 	}
+	
 	$status = updateActiveStatus($activeStatus);
 	echo $status;
 }
